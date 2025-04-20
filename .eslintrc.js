1e7a15c62b3eaf176b600ff2ce2838363de7ea3f
Kevin Mercado <kevinmercado192000@icloud.com>
2025-04-20 12:59:51 +0200

    Save

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,40 @@
+/* eslint-env node */
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    'jest/globals': true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
-  }
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'off',
+    'react/prop-types': 'off',
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'padded-blocks': ['error', 'never'],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+  },
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,62 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
-import { useApi } from './useApi'
-import LoadingSpinner from './LoadingSpinner'
-import ErrorMessage from './ErrorMessage'
-import PokemonPage from './PokemonPage'
-import PokemonList from './PokemonList'
+import React from 'react';
+import { Routes, Route, useMatch } from 'react-router-dom';
+import { useApi } from './useApi';
+import LoadingSpinner from './LoadingSpinner';
+import ErrorMessage from './ErrorMessage';
+import PokemonPage from './PokemonPage';
+import PokemonList from './PokemonList';
 
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+const mapResults = ({ results }) =>
+  results.map(({ url, name }) => ({
+    url,
+    name,
+    id: parseInt(url.match(/\/(\d+)\//)[1]),
+  }));
 
 const App = () => {
-  const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  const match = useMatch('/pokemon/:name');
+  const {
+    data: pokemonList,
+    error,
+    isLoading,
+  } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults);
 
   if (isLoading) {
-    return <LoadingSpinner />
-  }
-  if (error) {
-    return <ErrorMessage error={error} />
+    return <LoadingSpinner />;
   }
 
-  let next = null
-  let previous = null
+  if (error) {
+    return <ErrorMessage error={error} />;
+  }
+
+  let next = null;
+  let previous = null;
 
   if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
+    const current = pokemonList.find(({ name }) => name === match.params.name);
+
+    if (current) {
+      const pokemonId = current.id;
+      previous = pokemonList.find(({ id }) => id === pokemonId - 1) || null;
+      next = pokemonList.find(({ id }) => id === pokemonId + 1) || null;
+    }
   }
 
   return (
     <Routes>
-      <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route exact path="/pokemon/:name" element={
-        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-      } />
+      <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+      <Route
+        path="/pokemon/:name"
+        element={
+          <PokemonPage
+            pokemonList={pokemonList}
+            previous={previous}
+            next={next}
+          />
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
