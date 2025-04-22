08619fa7405c9acd789e950d198c1b6a4b2a295c
Kevin Mercado <kevinmercado192000@icloud.com>
2025-04-22 13:06:54 +0200

    finish2

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e-tests',
+  testMatch: '**/*.test.js',
   webServer: {
     command: 'npm run start-prod',
     port: 3000,
