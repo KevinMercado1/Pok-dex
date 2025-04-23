55d07ab792945863c9fc8a8198d0aab28b8383a9
Kevin Mercado <kevinmercado192000@icloud.com>
2025-04-23 21:40:38 +0200

    ok

--- playwright.config.js
+++ playwright.config.js
@@ -4,9 +4,7 @@ export default defineConfig({
   testDir: './e2e-tests',
   testMatch: '**/*.test.js',
   webServer: {
-    command: 'npm run start-prod',
     port: 3000,
-    timeout: 120 * 1000,
     reuseExistingServer: true,
   },
 });
