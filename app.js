d33436cbeb16307a7135cbe294625fa170e59a26
Kevin Mercado <kevinmercado192000@icloud.com>
2025-05-06 07:48:10 +0200

    Added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
-// get the port from env variable
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok');
 });
