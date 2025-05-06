277bda4131a9ddd23b935578eec96dddd0e84f95
Kevin Mercado <kevinmercado192000@icloud.com>
2025-05-06 14:02:32 +0200

    ok

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // get the port from env variable
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('dist'));
+app.use(express.static('build'));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/health', (req, res) => {
